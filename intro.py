91c525c6cd6bbd90c08af3646d28a21d0afc669d
mikecinnamon <cinndata@gmail.com>
2024-09-17 19:35:38 +0200

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,66 @@
+## [PY-01] Introduction to Python ##
+
+# Typing Python code #
+2 + 2
+a = 2 + 2
+a
+a = 7 - 2
+a
+b = 2 * 3
+b - 1
+b**2
+
+# Python packages #
+import math
+math.sqrt(2)
+from math import sqrt
+sqrt(2)
+
+# Numeric types #
+type(a)
+type(2.5)
+type(2)
+type(2.0)
+2 + 2.5
+
+# Boolean data #
+d = 5 < a
+d
+type(d)
+a == 4
+math.sqrt(d)
+1 - d
+
+# Strings #
+c = 'Messi'
+type(c)
+'Leo' + ' ' + 'Messi'
+
+# Lists #
+mylist = ['Messi', 'Cristiano', 'Neymar', 'Mbappé']
+len(mylist)
+newlist = mylist + [2, 3]
+newlist
+len(newlist)
+mylist[0:2]
+mylist[2:]
+mylist[:3]
+
+# Ranges #
+myrange = range(0, 10, 2)
+list(myrange)
+list(range(5, 12))
+list(range(10))
+
+# Dictionaries #
+my_dict = {'name': 'Joan', 'gender': 'F', 'age': 32}
+
+# Functions #
+def f(x):
+    y = 1/(1 - x**2)
+    return y
+f(2)
+f(1)
+f('Mary')
+def g(x, y): return x*y/(x**2 + y**2)
+g(1, 1)
